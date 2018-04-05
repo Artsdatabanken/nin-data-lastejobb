faa8d2dbba260f87fac75ceda3130d4ebcd93593
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-04-05 08:36:05 +0200

    Les inn bounding bokser
    Fixes #36

--- config/datakilde.js
+++ config/datakilde.js
@@ -4,7 +4,8 @@ const datakilde = {
   NA_grunntyper: './kildedata/NA_grunntyper.json',
   MI_variasjon:
     'http://webtjenester.artsdatabanken.no/NiN/v2b/variasjon/alleKoder',
-  OR_organisasjon: './kildedata/organisasjon.json'
+  OR_organisasjon: './kildedata/organisasjon.json',
+  bbox: './kildedata/bbox.json'
 }
 
 module.exports = datakilde
