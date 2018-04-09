c0bbb47485f1a837bc1abd79074edb4778e10d4a
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-04-09 11:53:27 +0200

    Logging med tidsmåling og bedre formattering (#50)
    * Logging med tidsmåling og bedre formattering
    
    * Add prettier lint-stage
    
    * Readd build script

--- steg/1_nedlasting/AO_kommune.js
+++ steg/1_nedlasting/AO_kommune.js
@@ -1,78 +1,83 @@
-const io = require('../../lib/io')
-const http = require('../../lib/http')
-const log = require('../../lib/log')
-const config = require('../../config')
+const path = require("path");
+const io = require("../../lib/io");
+const http = require("../../lib/http");
+const log = require("log-less-fancy")();
+const config = require("../../config");
 
 function parseSpråk(s) {
   switch (s) {
-    case 'Gáivuotna - Kåfjord - Kaivuono':
-      return 'Kåfjord'
-    case 'Fauske - Fuosko':
-      return 'Fauske'
-    case 'Hamarøy - Hábmer':
-      return 'Hamarøy'
-    case 'Divtasvuodna - Tysfjord':
-      return 'Tysfjord'
-    case 'Sortland - Suortá':
-      return 'Sortland'
-    case 'Harstad - Hárstták':
-      return 'Harstad'
-    case 'Loabák - Lavangen':
-      return 'Lavangen'
-    case 'Storfjord - Omasvuotna - Omasvuono':
-      return 'Storfjord'
-    case 'Gáivuotna - Kåfjord - Kaivuono':
-      return 'Kåfjord'
-    case 'Guovdageaidnu - Kautokeino':
-      return 'Kautokeino'
-    case 'Porsanger - Porsángu - Porsanki':
-      return 'Porsanger'
-    case 'Kárášjohka - Karasjok':
-      return 'Karasjok'
-    case 'Deatnu - Tana':
-      return 'Tana'
-    case 'Unjárga - Nesseby':
-      return 'Nesseby'
-    case 'Snåase - Snåsa':
-      return 'Snåsa'
-    case 'Raarvihke - Røyrvik':
-      return 'Røyrvik'
+    case "Gáivuotna - Kåfjord - Kaivuono":
+      return "Kåfjord";
+    case "Fauske - Fuosko":
+      return "Fauske";
+    case "Hamarøy - Hábmer":
+      return "Hamarøy";
+    case "Divtasvuodna - Tysfjord":
+      return "Tysfjord";
+    case "Sortland - Suortá":
+      return "Sortland";
+    case "Harstad - Hárstták":
+      return "Harstad";
+    case "Loabák - Lavangen":
+      return "Lavangen";
+    case "Storfjord - Omasvuotna - Omasvuono":
+      return "Storfjord";
+    case "Gáivuotna - Kåfjord - Kaivuono":
+      return "Kåfjord";
+    case "Guovdageaidnu - Kautokeino":
+      return "Kautokeino";
+    case "Porsanger - Porsángu - Porsanki":
+      return "Porsanger";
+    case "Kárášjohka - Karasjok":
+      return "Karasjok";
+    case "Deatnu - Tana":
+      return "Tana";
+    case "Unjárga - Nesseby":
+      return "Nesseby";
+    case "Snåase - Snåsa":
+      return "Snåsa";
+    case "Raarvihke - Røyrvik":
+      return "Røyrvik";
   }
-  return s
+  return s;
 }
 
 function mapKommuner(kommuner) {
-  r = {}
+  r = {};
   kommuner.forEach(ci => {
     // AO_18-50
-    const kode = config.kodesystem.prefix.administrativtOmråde + ci.code
-    if (ci.code !== '9999') {
+    const kode =
+      config.kodesystem.prefix.administrativtOmråde +
+      ci.code.substring(0, 2) +
+      "-" +
+      ci.code.substring(2);
+    if (ci.code !== "9999") {
       r[kode] = {
         kode: kode,
         tittel: { nb: parseSpråk(ci.name) }
-      }
+      };
     }
-  })
-  return r
+  });
+  return r;
 }
 
 async function importKommuner() {
-  var dato = new Date()
-  const from = dato.toISOString().substring(0, 10) // 2018-01-30
-  dato.setDate(dato.getDate() + 1)
-  const to = dato.toISOString().substring(0, 10) // 2018-01-31
+  var dato = new Date();
+  const from = dato.toISOString().substring(0, 10); // 2018-01-30
+  dato.setDate(dato.getDate() + 1);
+  const to = dato.toISOString().substring(0, 10); // 2018-01-31
 
   let kommuner = await http.getJsonFromCache(
     `http://data.ssb.no/api/klass/v1/classifications/131/codes.json?from=${from}&to=${to}`,
-    config.getCachePath('ssb') + 'kommuner.json'
-  )
-  return mapKommuner(kommuner)
+    config.getCachePath("ssb") + "kommuner.json"
+  );
+  return mapKommuner(kommuner);
 }
 
 importKommuner()
   .then(r => {
-    io.writeJson(config.getDataPath(__filename), r)
+    io.writeJson(config.getDataPath(__filename), r);
   })
   .catch(err => {
-    process.exit(99)
-  })
+    process.exit(99);
+  });
--- steg/2_konvertering/AR_diagnostiske_arter.js
+++ steg/2_konvertering/AR_diagnostiske_arter.js
@@ -1,70 +1,72 @@
-const io = require('../../lib/io')
-const log = require('../../lib/log')
-const config = require('../../config')
-const { kodkode, splittKode, lookup } = require('../../lib/koder')
-const koder = require('../../lib/koder')
+if (!process.env.DEBUG) process.env.DEBUG = "*";
+const path = require("path");
+const io = require("../../lib/io");
+const log = require("log-less-fancy")();
+const config = require("../../config");
+const { kodkode, splittKode, lookup } = require("../../lib/koder");
+const koder = require("../../lib/koder");
 
-log.logLevel = 6
+log.logLevel = 6;
 
-let diagArt = io.readJson(config.datakilde.NA_diagnostisk_art)
-let arter = io.readJson(config.getDataPath('1_nedlasting/AR_taxon.json'))
-let nin_liste = io.readJson(config.getDataPath('1_nedlasting/NA_koder.json'))
+let diagArt = io.readJson(config.datakilde.NA_diagnostisk_art);
+let arter = io.readJson(config.getDataPath("1_nedlasting/AR_taxon.json"));
+let nin_liste = io.readJson(config.getDataPath("1_nedlasting/NA_koder.json"));
 
-let r = {}
+let r = {};
 
 function linkOne(nodeFra, nodeTil, funksjon, tag) {
   const variabel = funksjon
-    .replace(tag, '')
-    .replace('[', '')
-    .replace(']', '')
+    .replace(tag, "")
+    .replace("[", "")
+    .replace("]", "");
 
-  const kodeFra = nodeFra.kode
-  const kodeTil = nodeTil.kode
-  if (!r[kodeFra]) r[kodeFra] = { relasjon: {} }
-  const relasjon = r[kodeFra].relasjon
-  if (!relasjon[tag]) relasjon[tag] = {}
+  const kodeFra = nodeFra.kode;
+  const kodeTil = nodeTil.kode;
+  if (!r[kodeFra]) r[kodeFra] = { relasjon: {} };
+  const relasjon = r[kodeFra].relasjon;
+  if (!relasjon[tag]) relasjon[tag] = {};
   relasjon[tag][kodeTil] = {
     kode: kodeTil,
     tittel: nodeTil.tittel,
     variabel: variabel
-  }
+  };
 }
 
 function linkBoth(node1, node2, funksjon, tag) {
-  if (!tag) return
-  if (!funksjon) return
-  tag = tag.trim().replace(' ', '_')
-  funksjon = funksjon.trim()
-  linkOne(node1, node2, funksjon, tag)
-  linkOne(node2, node1, funksjon, tag)
+  if (!tag) return;
+  if (!funksjon) return;
+  tag = tag.trim().replace(" ", "_");
+  funksjon = funksjon.trim();
+  linkOne(node1, node2, funksjon, tag);
+  linkOne(node2, node1, funksjon, tag);
 }
 
-let ukjenteKoder = {}
+let ukjenteKoder = {};
 Object.keys(diagArt).forEach(key => {
-  const art = diagArt[key]
+  const art = diagArt[key];
   const hovedtype =
-    config.kodesystem.prefix.natursystem + art.Kartleggingsenhet.split('-')[0]
+    config.kodesystem.prefix.natursystem + art.Kartleggingsenhet.split("-")[0];
   const na_kode =
-    config.kodesystem.prefix.natursystem + art.Kartleggingsenhet.trim()
+    config.kodesystem.prefix.natursystem + art.Kartleggingsenhet.trim();
   if (!nin_liste[na_kode])
     ukjenteKoder[na_kode] = ukjenteKoder[na_kode]
       ? ukjenteKoder[na_kode] + 1
-      : 1
+      : 1;
   else {
-    const idkode = koder.artskode(art.scientificNameID, art.Scientificname)
+    const idkode = koder.artskode(art.scientificNameID, art.Scientificname);
     if (arter[idkode]) {
       //      const tx_kode = arter[idkode].se
-      const na = nin_liste[na_kode]
-      tx = arter[idkode]
-      if (tx.se) tx = arter[tx.se]
-      let e = {}
-      linkBoth(na, tx, art['Funksjon1'], art['tags1'])
-      linkBoth(na, tx, art['Funksjon2'], art['tags2'])
-      linkBoth(na, tx, art['Funksjon3'], art['tags3'])
-      linkBoth(na, tx, art['Funksjon 4'], art['tags4'])
-    } else log.w('Fant ikke art ' + idkode)
+      const na = nin_liste[na_kode];
+      tx = arter[idkode];
+      if (tx.se) tx = arter[tx.se];
+      let e = {};
+      linkBoth(na, tx, art["Funksjon1"], art["tags1"]);
+      linkBoth(na, tx, art["Funksjon2"], art["tags2"]);
+      linkBoth(na, tx, art["Funksjon3"], art["tags3"]);
+      linkBoth(na, tx, art["Funksjon 4"], art["tags4"]);
+    } else log.warn("Fant ikke art " + idkode);
   }
-})
+});
 
-log.w('Ukjente koder', ukjenteKoder)
-io.writeJson(config.getDataPath(__filename), r)
+log.warn("Ukjente koder", ukjenteKoder);
+io.writeJson(config.getDataPath(__filename), r);
