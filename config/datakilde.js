3152eb464952c0bd8c5a1bb82262c1fc13efe4f9
Even <even.juberg@ntnu.no>
2018-04-17 11:57:50 +0200

    Endret datakilde http->https

--- config/datakilde.js
+++ config/datakilde.js
@@ -2,9 +2,9 @@ const datakilde = {
   AR_taxon_all: "https://www.artsdatabanken.no/api/taxon/all",
   AR_taxon: "https://www.artsdatabanken.no/api/taxon/?pageindex=1&pagesize=2",
   MI_variasjon:
-    "http://webtjenester.artsdatabanken.no/NiN/v2b/variasjon/alleKoder",
+    "https://webtjenester.artsdatabanken.no/NiN/v2b/variasjon/alleKoder",
   NA_diagnostisk_art: "NA_diagnostisk_art",
-  NA_koder: "http://webtjenester.artsdatabanken.no/NiN/v2b/koder/alleKoder",
+  NA_koder: "https://webtjenester.artsdatabanken.no/NiN/v2b/koder/alleKoder",
   NA_hovedtyper: "NA_hovedtyper",
   NA_grunntyper: "NA_grunntyper",
   OR_organisasjon: "OR_organisasjon",
