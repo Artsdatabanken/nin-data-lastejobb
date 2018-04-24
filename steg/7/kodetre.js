3f7887d50099305087fb2f6f2ef84936caf7507b
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-04-24 09:07:55 +0200

    Bygg kodetreet for GIS og API (#67)
    * Bygg kodetre
    
    Resolves #5
    
    * Update snapshots

---
+++ steg/7/kodetre.js
@@ -0,0 +1,38 @@
+const config = require("../../config")
+const io = require("../../lib/io")
+const log = require("log-less-fancy")()
+
+let data = io.lesDatafil("flett_metabase")
+
+function settInn(kode, forelder, tittel) {
+  r.push({ kode: kode, forelder: forelder, tittel: tittel })
+}
+
+let r = []
+Object.keys(data).forEach(key => {
+  const node = data[key]
+  if (node.se) {
+  } else {
+    const kode = key
+    let foreldre = node.foreldre
+    if (!foreldre) foreldre = []
+    if (kode === config.kodesystem.rotkode) settInn(kode, null, node.tittel)
+    if (foreldre.length > 0) {
+      foreldre.forEach(forelder => settInn(kode, forelder, node.tittel))
+    }
+  }
+})
+
+function finn(kode) {
+  return r.filter(node => node.kode === kode)
+}
+
+function valider(kode) {
+  const node = finn(kode)
+  if (!node) throw new Error("Mangler " + kode)
+  console.log(node)
+}
+
+valider("NA")
+
+io.skrivDatafil(__filename, r)
---
+++ steg/7/kodetre_postgre.js
@@ -0,0 +1,61 @@
+const config = require("../../config")
+const io = require("../../lib/io")
+const log = require("log-less-fancy")()
+
+const dummySuperRoot = "##"
+let data = io.lesDatafil("flett_metabase")
+let r = []
+
+Object.keys(data).forEach(forelder => {
+  const node = data[forelder]
+  node.barn = []
+})
+
+function dyttInn(kode) {
+  const node = data[kode]
+  node.kode = kode
+  node.tittel = node.tittel
+    ? node.tittel.nb || node.tittel.en || node.tittel.la
+    : "?"
+  let foreldre = node.foreldre
+  if (!foreldre) {
+    console.warn("Mangler forelder: " + kode)
+    return
+  }
+
+  foreldre.forEach(forelder => {
+    let fn = data[forelder]
+    if (!fn) {
+      console.warn("Finner ikke kode ", forelder)
+      return
+    }
+    fn.kode = forelder
+    fn.barn.push(node)
+  })
+}
+
+function eksporter(node, forfedre, nivå = 0) {
+  if (!node.barn) {
+    return
+  }
+  forfedre = Object.assign([], forfedre)
+  if (node.kode !== dummySuperRoot) forfedre.push(node.kode)
+  node.barn.forEach(b => {
+    const rel = {
+      kode: b.kode,
+      nivå: nivå,
+      forfedre: forfedre,
+      tittel: b.tittel
+    }
+    r.push(rel)
+    eksporter(b, forfedre, nivå + 1)
+  })
+}
+
+Object.keys(data).forEach(kode => {
+  dyttInn(kode)
+})
+
+eksporter({ kode: dummySuperRoot, barn: [data[config.kodesystem.rotkode]] }, [])
+
+io.skrivDatafil(__filename, r)
