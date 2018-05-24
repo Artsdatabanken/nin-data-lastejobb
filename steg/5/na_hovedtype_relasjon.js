24367eb29d74d351e3250bc050af85ff83cf5000
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-05-24 15:46:23 +0200

    Kobling fra hovedtyper til LKMer har falt ut (#131)
    * Kobling fra hovedtyper til LKMer
    
    * update snapshots

---
+++ steg/5/na_hovedtype_relasjon.js
@@ -0,0 +1,45 @@
+const io = require("../../lib/io")
+const log = require("log-less-fancy")()
+const config = require("../../config")
+const typesystem = require("@artsdatabanken/typesystem")
+
+let na = io.lesDatafil("na_hovedtype")
+let r = {}
+
+const lkmTekst = {
+  u: "underordnet miljøvariabel",
+  h: "hoved-miljøvariabel",
+  t: "tilleggs-miljøvariabel",
+  d: "differensierende miljøvariabel"
+}
+function relasjon(node, kant, kode, kantRetur = "hovedtype") {
+  node.relasjon.push({
+    kode: kode,
+    kant: kant,
+    kantRetur: kantRetur,
+    erSubset: true
+  })
+}
+
+function map(ht) {
+  let o = {
+    ingress: ht.ingress,
+    infoUrl: ht.infoUrl,
+    relasjon: []
+  }
+  relasjon(o, "definisjonsgrunnlag", ht.definisjonsgrunnlag.kode)
+  relasjon(o, "prosedyrekategori", ht.prosedyrekategori.kode)
+  if (!ht.lkm) return o
+  Object.keys(ht.lkm).forEach(lkmtype => {
+    ht.lkm[lkmtype].forEach(kode => {
+      relasjon(o, lkmTekst[lkmtype], kode)
+    })
+  })
+  return o
+}
+
+Object.keys(na).forEach(kode => {
+  r[kode] = map(na[kode])
+})
+
+io.skrivDatafil(__filename, r)
