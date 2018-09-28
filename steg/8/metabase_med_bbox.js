c9beebdf03c6142e57dce3c0595474efac788600
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-09-28 16:15:32 +0200

    Add meta info for available map formats

--- steg/8/metabase_med_bbox.js
+++ steg/8/metabase_med_bbox.js
@@ -3,14 +3,14 @@ const io = require("../../lib/io")
 const log = require("log-less-fancy")()
 
 let tre = io.lesDatafil("full_med_graf")
-let bboxFeatures = io.lesDatafil("inn_bbox").tilesets
-let mbtiles = io.lesKildedatafil("mbtiles")
+let mbtiles = io.lesDatafil("inn_mbtiles")
 
 function avrund1d(num) {
-  return Math.round(num * 1000) / 1000
+  return Math.round(parseFloat(num) * 1000) / 1000
 }
 
-function avrund4d(bbox) {
+function avrund4d(bounds) {
+  const bbox = bounds.split(",")
   const bboxjson = bbox.map(f => avrund1d(f))
   const ll = [bboxjson[1], bboxjson[0]]
   const ur = [bboxjson[3], bboxjson[2]]
@@ -19,22 +19,25 @@ function avrund4d(bbox) {
   return [ll, ur]
 }
 
-function settBbox(kode, bbox) {
-  const node = tre[kode]
-  node.bbox = avrund4d(bbox)
-}
-
-Object.keys(bboxFeatures).forEach(kode => {
-  const bbox = bboxFeatures[kode].bounds
-  if (tre[kode]) settBbox(kode, bbox)
-  else log.warn("bbox for kode '" + kode + "', men koden eksisterer ikke")
-})
-
-Object.keys(mbtiles).forEach(kode => {
-  const mbtile = mbtiles[kode]
-  if (tre[kode])
-    tre[kode].zoom = [parseInt(mbtile.minzoom), parseInt(mbtile.maxzoom)]
-  else log.warn("bbox for kode '" + kode + "', men koden eksisterer ikke")
+// Forventer følgende katalogstruktur på tile serveren:
+// /kartkategori/?/?/kode
+// Dvs. at rotkatalog betraktes som klasse av data, eks. gradient eller trinn
+Object.keys(mbtiles).forEach(path => {
+  const parts = path.split("/")
+  if (parts.length < 3) return
+  const klasse = parts[1]
+  const kode = parts[parts.length - 1]
+  const mbtile = mbtiles[path]
+  if (!tre[kode]) {
+    log.warn("bbox for kode '" + kode + "', men koden eksisterer ikke")
+    return
+  }
+  const target = tre[kode]
+  target.zoom = [parseInt(mbtile.minzoom), parseInt(mbtile.maxzoom)]
+  target.bbox = avrund4d(mbtile.bounds)
+  if (!target.formats) target.formats = {}
+  target.formats[klasse] = mbtile.format
+  if (kode == "BS_6SO") console.log(target)
 })
 
 io.skrivDatafil(__filename, tre)
