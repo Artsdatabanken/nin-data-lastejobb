aedea0fb46aaef42f5cabc2e6f15d750ec4d4175
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-10-10 14:03:30 +0200

    Add gradients for LA

--- steg/3/la.js
+++ steg/3/la.js
@@ -3,6 +3,7 @@ const io = require("../../lib/io")
 const typesystem = require("@artsdatabanken/typesystem")
 
 let hovedtyper = io.lesDatafil("la_to_json")
+let gradienter = io.lesKildedatafil("la_lg")
 
 const r = {}
 
@@ -10,6 +11,20 @@ hovedtyper.forEach(e => {
   r[e.kode] = {
     tittel: { nb: e.tittel }
   }
+})
+
+Object.keys(gradienter).forEach(kode => {
+  const gradKoder = gradienter[kode]
+  const relasjoner = []
+  gradKoder.forEach(gradKode => {
+    relasjoner.push({
+      kode: gradKode,
+      kant: "definert av",
+      kantRetur: "definerer"
+    })
+  })
+  r[kode].relasjon = relasjoner
+  console.log(r[kode])
 })
 
 io.skrivDatafil(__filename, r)
