c7fd62994dec295b85da980da853ce1014393bec
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-10-27 10:17:24 +0200

    Oppgradering av landskapskoder

--- steg/02/la_to_json.js
+++ steg/02/la_to_json.js
@@ -5,45 +5,46 @@ const io = require("../../lib/io")
 const log = require("log-less-fancy")()
 const config = require("../../config")
 
-const kildefil = config.kildedataPath + "/la.csv"
-const rs = fs.createReadStream(kildefil, "utf-8")
-const writePath = config.getDataPath(__filename)
-const ws = fs.createWriteStream(writePath)
-
-const csvOptions = {
-  delimiter: ",",
-  relax_column_count: true,
-  escape: "\\"
-}
-
-let header
-
-importCsv(kildefil)
-
-async function importCsv(kildefil) {
-  await rs
-    .pipe(csv.parse(csvOptions))
-    .pipe(csv.transform(transform))
-    .pipe(JSONStream.stringify())
-    .pipe(ws)
-}
-
-function lagKode(nøkkel) {
-  let kode = "LA_" + nøkkel[3]
-  const subkey = nøkkel.substring(4)
-  if (subkey) kode += "-" + subkey
-  return kode
-}
-
-function transform(record) {
-  if (!header) {
-    header = {}
-    for (let i = 0; i < record.length; i++) header[record[i]] = i
-    return
+class CsvToJson {
+  constructor(csvOptions) {
+    this.csvOptions = csvOptions || {
+      delimiter: "\t",
+      relax_column_count: true,
+      escape: "\\"
+    }
   }
 
-  return {
-    kode: lagKode(record[header["S_kode"]]),
-    tittel: record[header["Name"]].trim()
+  async convert(kildefil, writePath) {
+    this.header = null
+    const rs = fs.createReadStream(kildefil, "utf-8")
+    const ws = fs.createWriteStream(writePath)
+    await rs
+      .pipe(csv.parse(this.csvOptions))
+      .pipe(csv.transform(this.transform))
+      .pipe(JSONStream.stringify())
+      .pipe(ws)
   }
+
+  transform(record) {
+    if (!this.header) {
+      this.header = {}
+      for (let i = 0; i < record.length; i++)
+        this.header[i] = record[i].toLowerCase() || "field" + i
+      return
+    }
+
+    let r = {}
+    for (let i = 0; i < record.length; i++) r[this.header[i]] = record[i].trim()
+    return r
+  }
+}
+
+importer("la.csv")
+importer("la_klg.csv")
+
+function importer(csvFil) {
+  const kildefil = config.kildedataPath + "/" + csvFil
+  const writePath = config.getDataPath(csvFil + ".json")
+  log.info("Import " + csvFil + " to " + writePath)
+  new CsvToJson().convert(kildefil, writePath)
 }
