755926c0a9f8f347d4bea080cf4d5c25832a2e67
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-10-31 12:11:59 +0100

    Add unique color index for use in rasters

---
+++ steg/13/metabase_redis.js
@@ -0,0 +1,15 @@
+const config = require("../../config")
+const io = require("../../lib/io")
+const log = require("log-less-fancy")()
+const fs = require("fs")
+
+const REDIS = false
+if (!REDIS) return
+
+let data = io.lesDatafil("metabase")
+const dest = fs.createWriteStream("metabase.redis")
+Object.keys(data).forEach(key => {
+  const value = JSON.stringify(data[key]).replace("'", "\\'")
+  dest.write(`SET     ${key} '${value}'\n`)
+})
+dest.close()
