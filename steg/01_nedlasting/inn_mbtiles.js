7ae416f4923687333e2148b7bcec26a31daa31d0
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-10-31 11:19:10 +0100

    Reenable certificate checking

--- steg/01_nedlasting/inn_mbtiles.js
+++ steg/01_nedlasting/inn_mbtiles.js
@@ -2,9 +2,6 @@ const http = require("../../lib/http")
 const config = require("../../config")
 const log = require("log-less-fancy")()
 
-// TEMP TODO HACK - hydra cert invalid
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0"
-
 // Laster ned bounding bokser for koder
 http
   .downloadJson2File(config.datakilde.mbtiles, config.getDataPath(__filename))
