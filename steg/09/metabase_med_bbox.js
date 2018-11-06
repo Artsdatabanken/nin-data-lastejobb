cbf13e6341ec38f01b5c3fef9adbac20be87332f
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-11-07 00:52:13 +0100

    Fjern raster-index - hentes fra tileserver (#287)

--- steg/09/metabase_med_bbox.js
+++ steg/09/metabase_med_bbox.js
@@ -2,7 +2,7 @@ const config = require("../../config")
 const io = require("../../lib/io")
 const log = require("log-less-fancy")()
 
-let tre = io.lesDatafil("full_med_rid")
+let tre = io.lesDatafil("full_med_graf")
 let mbtiles = io.lesDatafil("inn_mbtiles")
 
 function avrund1d(num) {
