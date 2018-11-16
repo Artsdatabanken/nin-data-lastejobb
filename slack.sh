bd9a403b46fad737f57e995b2d768d890103052d
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-11-16 14:06:27 +0100

    I am not Anna, I am travis

--- slack.sh
+++ slack.sh
@@ -1,2 +1,1 @@
-curl -X POST --data-urlencode "payload={\"channel\": \"$1\", \"username\": \"anna\", \"text\": \"$2\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
-    
+curl -X POST --data-urlencode "payload={\"channel\": \"$1\", \"username\": \"travis not the band\", \"text\": \"$2\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
