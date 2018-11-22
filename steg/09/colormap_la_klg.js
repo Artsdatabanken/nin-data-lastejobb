84f3c00932ff2792832c58409cc7cb3f566a716f
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-11-22 15:03:59 +0100

    Add LA-KLG to the map

---
+++ steg/09/colormap_la_klg.js
@@ -0,0 +1,56 @@
+const Jimp = require("jimp")
+const io = require("../../lib/io")
+
+let koder = io.lesKildedatafil("la_index")
+let farger = io.lesDatafil("la_farger")
+let la = io.lesDatafil("la")
+let klg = io.lesDatafil("la_klg")
+
+Object.keys(klg).forEach(kode => lagPalett(kode))
+
+const lkm2type = lkmTilType()
+
+function lkmTilType() {
+  const r = {}
+  Object.keys(la).forEach(kode => {
+    const node = la[kode]
+    if (node.relasjon)
+      node.relasjon.forEach(rel => {
+        if (!r[rel.kode]) r[rel.kode] = []
+        r[rel.kode].push(kode)
+      })
+  })
+  return r
+}
+
+function lagPalett(kode) {
+  new Jimp(512, 1, 0xffffffff, (err, image) => {
+    fargeleggAlle(image, kode)
+    image.write("./data/" + kode + ".palette.png")
+  })
+}
+
+function fargeleggAlle(image, rotkode) {
+  Object.keys(klg).forEach(klgKode => {
+    console.log(rotkode, klgKode)
+    if (klgKode.startsWith(rotkode)) fargelegg(image, klgKode)
+  })
+}
+
+function fargelegg(image, klgKode) {
+  const typer = lkm2type[klgKode]
+  typer.forEach(type => {
+    const index = koder[type]
+    if (index) settFarge(image, type, index)
+  })
+}
+
+function settFarge(image, kode, x) {
+  if (!farger[kode]) {
+    console.log("Mangler farge: " + kode)
+    return
+  }
+  const color = Jimp.cssColorToHex(farger[kode].farge)
+  console.log(kode, x, color)
+  image.setPixelColor(color, x, 0)
+}
