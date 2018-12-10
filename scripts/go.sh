7223c5205bc6c7a0cab02a5b33f64871d8fe7b4c
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-12-10 09:30:00 +0100

    Mix child colors to create ancestor colors

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,6 +1,6 @@
 node steg/04/farger.js 
 node steg/08/la_farger.js 
-node steg/08/colormap.js 
+node steg/09/colormap.js 
 scp LA.palette.png grunnkart@hydra:tilesdata/indexed/
 scp LA.palette.png grunnkart@hydra:tilesdata/indexed/LA-KLG.palette.png
 source=LA.palette.png
--- scripts/wait.sh
+++ scripts/wait.sh
@@ -1,2 +1,2 @@
-while inotifywait -e close_write ./kildedata/farger.json; do ./go.sh; done
+while inotifywait -e close_write ./kildedata/farger.json; do ./scripts/go.sh; done
 
