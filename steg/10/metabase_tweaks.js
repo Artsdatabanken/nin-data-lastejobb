8d94f2d8b69e769dc5b747a8b98fb044823814fb
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-03 12:53:18 +0100

    Lister ikke alle verneområder under toppnode (#381)
    * LA-KF beskrives med fjordlandskap, ikke dallandskap
    
    * Maskinlesbare intervaller for landskapsgradienter (#377)
    
    * Lister ikke alle verneområder under toppnode

--- steg/10/metabase_tweaks.js
+++ steg/10/metabase_tweaks.js
@@ -178,7 +178,7 @@ function fjernEnkeltVerneområder(tre) {
   // Fjern barn fra VV - for mange, bruk alternative ruter
   const vv = tre.VV.barn
   const filter = /^VV-\d+$/
-  log.warn("VV n=", tre.VV.barn.length)
-  // TODO:
-  //  tre.VV.barn = vv.filter(x => !x.kode.match(filter))
+  Object.keys(vv).forEach(kode => {
+    if (kode.match(filter)) delete vv[kode]
+  })
 }
