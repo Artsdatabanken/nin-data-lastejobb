462b8444240205df9ac6d711ea40db9c20c25610
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-04 15:19:59 +0100

    Landskapsgradienter kart
    * Våtmarkspreg
    * Arealbruksintensitet kyst
    * Kystavstand
    * Jordbrukspreg

--- config/datakilde.js
+++ config/datakilde.js
@@ -11,6 +11,8 @@ const datakilde = {
     "http://trdefme02.miljodirektoratet.no/nedlasting/Naturbase/Shape/naturvern_utm33.zip",
   // https://kartkatalog.geonorge.no/metadata/kartverket/statistiske-inndelinger-lau/f7be9f83-e5e0-4914-8076-e37424221930
   ao_kommune_geom: "https://www.dropbox.com/s/cyi68l9mixg7bq7/LAU2018.zip?dl=1",
+  kommuner:
+    "https://nedlasting.geonorge.no/geonorge/Basisdata/Kommuner/GeoJSON/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.zip",
   statistikk: "https://maps.artsdatabanken.no/statistikk.json",
   mbtiles: "http://maps.artsdatabanken.no/index.json"
 }
