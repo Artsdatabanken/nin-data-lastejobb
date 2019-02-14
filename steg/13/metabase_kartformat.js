39fddf03b3af1f5bea4175687c980f088abcb2ea
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-14 20:52:08 +0100

    Legg til filstørrelse og sist oppdatert attributt (#394)
    * Legg til filstørrelse og sist oppdatert attributt
    
    * Legg til filnavn

--- steg/13/metabase_kartformat.js
+++ steg/13/metabase_kartformat.js
@@ -68,6 +68,9 @@ function addKartformat(source) {
     if (mapfile.maxzoom) {
       cv.zoom = [parseInt(mapfile.minzoom), parseInt(mapfile.maxzoom)]
     }
+    cv.filnavn = mapfile.filename
+    cv.størrelse = mapfile.size
+    cv.oppdatert = mapfile.mtime
     if (mapfile.bounds) {
       // For now, no bounds for GeoJSON
       cv.zoom = [parseInt(mapfile.minzoom), parseInt(mapfile.maxzoom)]
