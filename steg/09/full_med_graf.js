a060c13fdbb3001b822a06c652b799239c936103
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-17 14:41:28 +0100

    Lag fargekoder for NA LKM gradienter

--- steg/09/full_med_graf.js
+++ steg/09/full_med_graf.js
@@ -1,9 +1,15 @@
+const tinycolor = require("tinycolor2")
 const config = require("../../config")
 const io = require("../../lib/io")
 const log = require("log-less-fancy")()
+const typesystem = require("@artsdatabanken/typesystem")
 
 let full = io.lesDatafil("full")
+let hierarki = io.lesDatafil("kodehierarki")
+const barnAv = hierarki.barn
+
 Object.keys(full).forEach(kode => lagGrafkoblinger(kode, full[kode]))
+Object.keys(full).forEach(kode => lagGrafGradientkoblinger(kode, full[kode]))
 
 io.skrivDatafil(__filename, full)
 
@@ -47,4 +53,51 @@ function lagGrafkoblinger(kode, node) {
     if (e.kantRetur) lagGrafkobling(e.kode, kode, e.kantRetur, e, false)
   })
   delete node.relasjon
+}
+
+function supplerMedFarger(farge0, farge, barn) {
+  const f1 = new tinycolor(farge0)
+  const f = new tinycolor(farge)
+  for (let i = 0; i < barn.length; i++) {
+    const node = full[barn[i].kode]
+    if (node.farge) continue
+    const color = tinycolor.mix(f1, f, (100 * i) / (barn.length - 1))
+    node.farge = color.toHexString()
+  }
+}
+
+function lagGrafGradientkobling(kode, node, type, kantnode) {
+  const grkode0 = Object.keys(kantnode)[0]
+  const gradForelder = full[grkode0].foreldre[0]
+  const src = full[gradForelder]
+  if (src.type) log.warn(gradForelder, src.type)
+  if (src.type !== "gradient") return
+  let g = []
+  barnAv[gradForelder].forEach(bkode => {
+    const b = full[bkode]
+    g.push({
+      kode: bkode,
+      tittel: b.tittel,
+      farge: b.farge,
+      på: !!kantnode[bkode]
+    })
+  })
+  if (node.gradient === undefined) node.gradient = {}
+  if (node.gradient[type] === undefined) node.gradient[type] = []
+  g = g.sort((a, b) => (a.kode > b.kode ? 1 : -1))
+  supplerMedFarger(src.farge0, src.farge, g)
+  node.gradient[type] = {
+    kode: gradForelder,
+    url: node.url,
+    tittel: node.tittel,
+    trinn: g
+  }
+}
+
+function lagGrafGradientkoblinger(kode, node) {
+  if (!node.graf) return
+  Object.keys(node.graf).forEach(kant => {
+    const kantnode = node.graf[kant]
+    lagGrafGradientkobling(kode, node, kant, kantnode)
+  })
 }
