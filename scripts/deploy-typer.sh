ad6bcfc8607344a7953b5b56fffb6db55e6e44ec
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-12 20:58:53 +0100

    habitat -> Habitat (#466)

--- scripts/deploy-typer.sh
+++ scripts/deploy-typer.sh
@@ -1,5 +1,6 @@
 scp build/full-text-index.json grunnkart@hydra:/dockerdata/nin-lookup-api/
 scp build/metabase.json grunnkart@hydra:~/
+ssh grunnkart@hydra node extract-meta.js metabase.json
 scp build/NN-LA.json grunnkart@hydra:~/tilesdata/Natur_i_Norge/Landskap/metadata_med_undertyper.json
 scp build/NN-NA.json grunnkart@hydra:~/tilesdata/Natur_i_Norge/Natursystem/metadata_med_undertyper.json
 scp build/AO.json grunnkart@hydra:~/tilesdata/Fylke/metadata_med_undertyper.json
@@ -8,6 +9,5 @@ scp build/VV.json grunnkart@hydra:~/tilesdata/Naturvernområde/metadata_med_unde
 scp build/OR.json grunnkart@hydra:~/tilesdata/Datakilde/metadata_med_undertyper.json
 scp build/metabase.json grunnkart@hydra:~/tilesdata/metadata_med_undertyper.json
 
-ssh grunnkart@hydra node extract-meta.js metabase.json
 ssh grunnkart@hydra docker restart nin-lookup-api
 
