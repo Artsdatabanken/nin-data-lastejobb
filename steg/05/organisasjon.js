e0729bc6be3b0c26007031e8c5d74554bf88005d
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-25 10:09:37 +0100

    Lisenser og lenker til datasett på Geonorge
    - Kloner nå data fra https://github.com/Artsdatabanken/nin-data
    - Leser CSV direkte
    - Mapper dynamisk kolonner fra CSV-fil. Nye kolonner resulterer i nye nøkler

--- steg/05/organisasjon.js
+++ steg/05/organisasjon.js
@@ -2,26 +2,39 @@ const io = require("../../lib/io")
 const log = require("log-less-fancy")()
 const config = require("../../config")
 const typesystem = require("@artsdatabanken/typesystem")
+const csv = require("../../lib/csv")
 
+const organisasjonTilKode = {}
 let organisasjon = io.lesKildedatafil("Datakilde/organisasjon")
-let datasett = io.lesKildedatafil("Datakilde/datasett")
+Object.entries(organisasjon).forEach(([kode, o]) => {
+  organisasjonTilKode[o.tittel.nb] = kode
+  organisasjonTilKode[kode] = kode
+  organisasjonTilKode[kode.replace("OR-", "")] = kode
+})
 
-function lagRelasjonTilDatasett(kilde) {
-  Object.keys(datasett).forEach(key => {
-    const o = organisasjon[key]
-    const sett = datasett[key]
-    o.relasjon = sett.map(s => {
-      return {
-        kode: s,
-        kant: "Datasett",
-        kantRetur: "Datakilde",
-        kantReturFraAlleBarna: true,
-        erSubset: true
-      }
+let datasett = csv.les("./nin-data/Natur_i_Norge/datasett.csv")
+// Sorter slik at mer spesifikke koder kommer sist og overstyrer generell datakilde
+datasett.sort((a, b) => a.Datasett.length - b.Datasett.length)
+lagRelasjonTilDatasett(datasett)
+io.skrivDatafil(__filename, organisasjon)
+
+function lagRelasjonTilDatasett(datasett) {
+  datasett.forEach(ds => {
+    const { Datasett: kode, Dataleverandør, ...datasett } = ds
+    const orgkode = organisasjonTilKode[Dataleverandør]
+    if (!orgkode) throw new Error("Ukjent dataleverandør: ", Dataleverandør)
+    const o = organisasjon[orgkode]
+    if (!o.relasjon) o.relasjon = []
+    const rel = {
+      kode: kode,
+      kant: "Datasett",
+      kantRetur: "Datakilde",
+      kantReturFraAlleBarna: true,
+      erSubset: true
+    }
+    Object.entries(datasett).forEach(([key, value]) => {
+      rel[key.toLowerCase()] = value
     })
+    o.relasjon.push(rel)
   })
 }
-
-lagRelasjonTilDatasett()
-
-io.skrivDatafil(__filename, organisasjon)
