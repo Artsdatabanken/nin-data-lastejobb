007578d24bc86dd4ec952955dc60ab3047d6f9ef
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-29 11:42:48 +0100

    Legg til foto banner-bilde for data-portal (#512)
    * Bilder på typeinndeling
    
    * Reservebilder også for kataloger uten data
    
    * Legg til banner-bilde for data-portal

--- steg/15/metabase_bilder.js
+++ steg/15/metabase_bilder.js
@@ -12,15 +12,23 @@ Object.keys(full).forEach(kode => {
   const filer = filindeks[node.url]
   node.foto = node.foto || {}
   if (!filer) return
-  if (kode == "NN-NA") debugger
-  const forside = filer["forside_408.jpg"] || filer["forside_408.png"]
-  if (!forside) return
-  node.foto.forside = {
-    url: baseUrl + node.url + "/" + forside.filename,
+  if (kode == "NN-LA-KLG-REIA") debugger
+  add(filer, node, "forside", 408)
+  add(filer, node, "banner", 950)
+  // TODO: temp fallback
+  if (!node.banner && node.forside) node.banner = node.forside
+})
+
+function add(filer, node, tag, width) {
+  const basename = "forside_" + width
+  const imgfile = filer[basename + ".jpg"] || filer[basename + ".png"]
+  if (!imgfile) return
+  node.foto[tag] = {
+    url: baseUrl + node.url + "/" + imgfile.filename,
     lisens: "",
     opphav: "",
     utgiver: ""
   }
-})
+}
 
 io.skrivDatafil(__filename, full)
