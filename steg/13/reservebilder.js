84feb06b3e4c39bbecc7fa324047ce7b0450c487
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-29 13:12:49 +0100

    Bruk reservebilder også for bannere (#513)
    * Bilder på typeinndeling
    
    * Reservebilder også for kataloger uten data
    
    * Legg til banner-bilde for data-portal
    
    * Bruk reservebilder også for bannere

--- steg/13/reservebilder.js
+++ steg/13/reservebilder.js
@@ -12,37 +12,39 @@ let script = []
 // Forventer følgende katalogstruktur på tile serveren:
 // /type/subtype/.../format.projeksjon.filtype
 // Dvs. at rotkatalog betraktes som klasse av data, eks. gradient eller trinn
-finnReserverbilder()
-finnBilderSomKanGjenbrukesForLandskap()
+finnReserverbilder("forside_408")
+finnReserverbilder("forside_950")
+finnBilderSomKanGjenbrukesForLandskap("forside_408")
+finnBilderSomKanGjenbrukesForLandskap("forside_950")
 io.skrivDatafil(__filename, script)
 
-function finnBilderSomKanGjenbrukesForLandskap() {
+function finnBilderSomKanGjenbrukesForLandskap(basename) {
   const map = io.lesDatafil("landskap_bilder_som_gjenbrukes.csv.json")
   map.forEach(e => {
     const mål = tre["NN-LA-" + e.på]
     const kilde = tre[e["bruk bilde fra"].replace("LA-", "NN-LA-TI-")]
-    const målbilde = bildefil(mål.url)
+    const målbilde = bildefil(mål.url, basename)
     if (målbilde) return
-    const kildebilde = bildefil(kilde.url)
+    const kildebilde = bildefil(kilde.url, basename)
     if (!kildebilde) return
     dupliser(kilde.url, kildebilde, mål.url)
   })
 }
 
-function bildefil(url) {
+function bildefil(url, basename) {
   const maps = filindeks[url]
   if (!maps) return null
-  if (maps["forside_408.jpg"]) return "forside_408.jpg"
-  if (maps["forside_408.png"]) return "forside_408.png"
+  if (maps[basename + ".jpg"]) return basename + ".jpg"
+  if (maps[basename + ".png"]) return basename + ".png"
   return null
 }
 
-function finnReserverbilder() {
+function finnReserverbilder(basename) {
   Object.keys(tre).forEach(xkode => {
     if (xkode === "~") debugger
     const node = tre[xkode]
     const maps = filindeks[node.url]
-    if (maps && maps["forside_408.jpg"]) return // Already have an image
+    if (maps && maps[basename + ".jpg"]) return // Already have an image
     const barn = barnAv[xkode]
     if (!barn) return
     barn.sort((a, b) => a > b)
@@ -51,7 +53,7 @@ function finnReserverbilder() {
       const burl = bn.url
       const filer = filindeks[burl]
       if (!filer) continue
-      const bilde = filer["forside_408.png"] || filer["forside_408.jpg"]
+      const bilde = filer[basename + ".png"] || filer[basename + ".jpg"]
       if (!bilde) return
       dupliser(burl, bilde.filename, node.url)
       break
