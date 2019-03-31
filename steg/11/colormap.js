1b1593d8c73fc6fc7895376f8de545cbb02014f9
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-31 19:17:28 +0200

    Sørg for at alle landskapsfarger kan leses (#522)

---
+++ steg/11/colormap.js
@@ -0,0 +1,38 @@
+const Jimp = require("jimp")
+const io = require("../../lib/io")
+
+let koder = io.lesKildedatafilOld("Natur_i_Norge/Landskap/la_index")
+let meta = io.lesDatafil("metabase_med_farger")
+
+new Jimp(512, 8, 0xff0000ff, (err, image) => {
+  const color = Jimp.cssColorToHex("#00f")
+  for (let y = 0; y < 8; y++) image.setPixelColor(color, 0, y)
+  for (let nivå = 1; nivå < 9; nivå++) fyllNivå(nivå, image, nivå - 1)
+  image.write("LA.palette.png")
+})
+
+function fyllNivå(nivå, image, y) {
+  Object.keys(koder).forEach(kode => {
+    const index = koder[kode]
+    let dybde = 0
+    //    if (kode === "NN-LA-TI-M-A" && nivå === 4) debugger
+    while (true) {
+      dybde = kode.split("-").length
+      if (dybde <= nivå) break
+      const xoo = meta[kode]
+      const oo = meta[kode].foreldre
+      kode = oo[0]
+    }
+    settFarge(image, kode, index, y)
+  })
+}
+function settFarge(image, kode, x, y) {
+  if (!meta[kode]) {
+    console.log("Mangler farge: " + kode)
+    return
+  }
+  const farge = meta[kode].farge
+  if (kode.startsWith("NN-LA-TI-M")) console.log(y, x, farge, kode)
+  const color = Jimp.cssColorToHex(farge)
+  image.setPixelColor(color, x, y)
+}
