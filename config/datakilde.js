5e9084e21c86cc3d83f1a240e3b1d82a3669ffc8
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-16 13:46:52 +0200

    Fjern gammel datakilde

--- config/datakilde.js
+++ config/datakilde.js
@@ -9,9 +9,6 @@ const datakilde = {
   vv_verneområe:
     "http://trdefme02.miljodirektoratet.no/nedlasting/Naturbase/Shape/naturvern_utm33.zip",
   // https://kartkatalog.geonorge.no/metadata/kartverket/statistiske-inndelinger-lau/f7be9f83-e5e0-4914-8076-e37424221930
-  ao_kommune_geom: "https://www.dropbox.com/s/cyi68l9mixg7bq7/LAU2018.zip?dl=1",
-  kommuner:
-    "https://nedlasting.geonorge.no/geonorge/Basisdata/Kommuner/GeoJSON/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.zip",
   statistikk: "https://data.artsdatabanken.no/statistikk.json",
   filindeks: "http://data.artsdatabanken.no/index.json"
 }
