0d55dda5abe26a4b15edd8a56b1bb96fada5cd58
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-03 14:14:07 +0200

    Last ned kommunedata i kommune

--- steg/01_nedlasting/inn_kommune.js
+++ steg/01_nedlasting/inn_kommune.js
@@ -2,4 +2,4 @@ const config = require("../../config")
 const git = require("../../lib/git")
 
 // Download "Natur i Norge" data kildedata - fylke og kommune
-git.clone("https://github.com/Artsdatabanken/kommune-data.git", "kommune-data")
+git.clone("https://github.com/Artsdatabanken/kommune-data.git", "kommune")
