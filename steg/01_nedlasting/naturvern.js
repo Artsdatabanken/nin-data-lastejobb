befceb4f24115a7df0d0652f140e8d6c1bbf1f84
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-05 13:32:07 +0200

    Debug logging

--- steg/01_nedlasting/naturvern.js
+++ steg/01_nedlasting/naturvern.js
@@ -1,4 +1,5 @@
-const { git } = require("lastejobb")
+const { git, log } = require("lastejobb")
 
 // Download "Natur i Norge" data kildedata - naturvernområder
+log.info("Git clone naturvern")
 git.clone("https://github.com/Artsdatabanken/naturvern.git", "naturvern")
