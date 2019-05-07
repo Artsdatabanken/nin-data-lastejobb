6898da0f771fdc9193584c3637f06d2d3ca59ad9
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-07 09:27:59 +0200

    Bilder med link til originalbildet med metadata (mellomsteg)

--- steg/15/metabase_bilder.js
+++ steg/15/metabase_bilder.js
@@ -1,6 +1,5 @@
-const config = require("../../config")
-const io = require("../../lib/io")
-const log = require("log-less-fancy")()
+const { io } = require("lastejobb")
+const path = require("path")
 
 const baseUrl = "https://data.artsdatabanken.no/"
 
@@ -14,19 +13,64 @@ Object.keys(full).forEach(kode => {
   if (!filer) return
   add(filer, node, "forside", 408)
   add(filer, node, "banner", 950)
+  addKilde(node)
+  addBilder(node, filer)
   // TODO: temp fallback
   if (!node.banner && node.forside) node.banner = node.forside
 })
+
+function addBilder(node, filer) {
+  node.bilde = node.bilde || {}
+  const bilde = node.bilde
+  Object.keys(filer).forEach(fil => {
+    const p = path.parse(fil)
+    if (".png.jpg".indexOf(p.ext) < 0) return
+    const parts = p.name.split("_")
+    let [format, width] = parts
+    switch (width) {
+      case "40":
+        format = "avatar"
+        break
+      case "408":
+        format = "foto"
+        break
+      case "950":
+        format = "banner"
+        break
+    }
+    const mk = format
+    bilde[mk] = bilde[mk] || {}
+    bilde[mk].url = fil
+  })
+}
+
+function addKilde(node) {
+  node.bilde = node.bilde || {}
+  const bilde = node.bilde
+  if (!node.mediakilde) return
+  Object.keys(node.mediakilde).forEach(mk => {
+    let kilde = node.mediakilde[mk]
+    if (Array.isArray(kilde)) kilde = kilde[0]
+    if (!kilde) return
+    bilde[mk] = bilde[mk] || {}
+    bilde[mk].kildeurl = urlMedBildeOgMetadata(kilde)
+  })
+}
+
+function urlMedBildeOgMetadata(url) {
+  if (url.indexOf("commons.wikimedia.org") < 0) return url
+  // https://www.wikidata.org/wiki/Q1789866#/media/File:Seierstad_J%C3%B8a.jpg
+  // https://commons.wikimedia.org/wiki/File:Seierstad_J%C3%B8a.jpg#/media/File:Seierstad_J%C3%B8a.jpg
+  const file = url.split("/").pop()
+  return `https://commons.wikimedia.org/wiki/File:${file}#/media/File:${file}`
+}
 
 function add(filer, node, tag, width) {
   const basename = "forside_" + width
   const imgfile = filer[basename + ".jpg"] || filer[basename + ".png"]
   if (!imgfile) return
   node.foto[tag] = {
-    url: baseUrl + node.url + "/" + imgfile.filename,
-    lisens: "",
-    opphav: "",
-    utgiver: ""
+    url: baseUrl + node.url + "/" + imgfile.filename
   }
 }
 
