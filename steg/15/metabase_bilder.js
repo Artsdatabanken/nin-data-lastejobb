b54e5c058d08ef89aa22601e5aacb7dfd5ce9bda
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-13 11:23:23 +0200

    Mediakildeattributtene opp på rotnivå

--- steg/15/metabase_bilder.js
+++ steg/15/metabase_bilder.js
@@ -6,9 +6,10 @@ let r = []
 Object.keys(full).forEach(kode => {
   const node = full[kode]
   if (!node.mediakilde) return
-  r.push({ kode: kode, mediakilde: node.mediakilde })
+  r.push({ kode: kode, ...node.mediakilde })
   delete node.mediakilde
 })
 
 io.skrivDatafil("metabase_bilder", full)
+r.sort((a, b) => (a.kode > b.kode ? 1 : -1))
 io.skrivBuildfil("mediakilde", r)
