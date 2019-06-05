5588782569c8e49db84488f46098b17f13b3c1bf
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-06-06 00:53:37 +0200

    Filindeks: Nytt format

--- steg/01_nedlasting/inn_filindeks.js
+++ steg/01_nedlasting/inn_filindeks.js
@@ -2,8 +2,6 @@ const { http, log } = require("lastejobb")
 const config = require("../../config")
 
 // Laster ned bounding bokser for koder
-http
-  .downloadJson(config.datakilde.filindeks, "inn_filindeks.json")
-  .catch(err => {
-    log.fatal(err)
-  })
+http.downloadJson(config.datakilde.filindeks, "filindeks.json").catch(err => {
+  log.fatal(err)
+})
