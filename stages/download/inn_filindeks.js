5635a9ce87e7d2e8903f6d1aaf9919c828aee4d1
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-02 13:09:41 +0200

    Les indeks fra staging server

--- stages/download/inn_filindeks.js
+++ stages/download/inn_filindeks.js
@@ -2,7 +2,10 @@ const { http, log } = require("lastejobb")
 
 // Laster ned bounding bokser for koder
 http
-  .downloadJson("http://data.artsdatabanken.no/index.json", "filindeks.json")
+  .downloadJson(
+    "http://data.test.artsdatabanken.no/index.json",
+    "filindeks.json"
+  )
   .catch(err => {
     log.fatal(err)
   })
