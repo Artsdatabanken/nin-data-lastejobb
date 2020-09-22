031f0b19d02a203b86a87026e71696f08a667fca
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-09-22 10:46:03 +0200

    Fylker nå i Administrativ_grense

--- stages/deploy/staging.sh
+++ stages/deploy/staging.sh
@@ -6,7 +6,7 @@ scp scripts/extract-meta.js grunnkart@hydra:~/
 ssh grunnkart@hydra node --max_old_space_size=28192 extract-meta.js metabase.json
 scp build/NN-LA.json grunnkart@hydra:~/tilesdata/Natur_i_Norge/Landskap/metadata_med_undertyper.json
 scp build/NN-NA.json grunnkart@hydra:~/tilesdata/Natur_i_Norge/Natursystem/metadata_med_undertyper.json
-scp build/AO.json grunnkart@hydra:~/tilesdata/Fylke/metadata_med_undertyper.json
+scp build/AO.json grunnkart@hydra:~/tilesdata/Administrativ_grense/Territorialområde/Fastlands-Norge/metadata_med_undertyper.json
 scp build/AR.json grunnkart@hydra:~/tilesdata/Biota/metadata_med_undertyper.json
 scp build/VV.json grunnkart@hydra:~/tilesdata/Naturvernområde/metadata_med_undertyper.json
 scp build/OR.json grunnkart@hydra:~/tilesdata/Datakilde/metadata_med_undertyper.json
