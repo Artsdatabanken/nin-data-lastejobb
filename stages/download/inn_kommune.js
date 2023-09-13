80cd069a7c4bcfadd35483a7cfa66c9d58213880
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2023-09-13 11:31:19 +0200

    cleanup

--- stages/download/inn_kommune.js
+++ stages/download/inn_kommune.js
@@ -1,4 +1,21 @@
-const { git } = require("@artsdatabanken/lastejobb")
+const { config, http, processes } = require("@artsdatabanken/lastejobb")
+const path = require('path')
+
+async function untar(tarFileName) {
+    const isGzipped = path.extname(tarFileName) === '.gz'
+    await processes.exec('tar', [
+        '-xvf' + (isGzipped ? 'z' : ''), config.getTempPath(tarFileName),
+        '--one-top-level',
+        '-C ' + config.getTempPath(),
+    ]);
+}
+
+async function downloadAndUntar(url) {
+    const tarFileName = path.basename(new URL(url).pathname);
+    await http.downloadBinary(url, tarFileName);
+    untar(tarFileName)
+}
 
 // Download "Natur i Norge" data kildedata - fylke og kommune
-git.clone("https://github.com/Artsdatabanken/kommune.git", "temp/kommune")
+//git.clone("https://github.com/Artsdatabanken/kommune.git", "temp/kommune")
+downloadAndUntar("https://github.com/Artsdatabanken/kommune/releases/latest/download/artifacts.tar")
